4d8228602c233724657cb28718e381aeb02b5b60
myata64 <sergiolaconnelly@B450M-DS3H>
2023-06-07 00:03:29 +0300

    try to make reg

--- credo/urls.py
+++ credo/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+
+app_name = 'account'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', views.account, name='account'),
 ]
